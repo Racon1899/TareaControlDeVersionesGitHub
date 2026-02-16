a339d12354dcf1481e5a9f5bfd37f005578e7cec
usuario <usuario@10.0.2.15>
2026-02-16 18:44:45 +0100

    Modificacion nombre

--- TareaControlDeVersionesGitHub/src/TareaControlDeVersionesGitHub/Main.java
+++ TareaControlDeVersionesGitHub/src/TareaControlDeVersionesGitHub/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        String nombre = "Ana";
+        String nombre = "Francisco";
 
         saludar(nombre);
         System.out.println("Número de caracteres: " + contarCaracteres(nombre));
